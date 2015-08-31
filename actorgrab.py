c93ee61aa37323a957a44a978629742c2c2b9ace
mgaulton <mgaulton@users.noreply.github.com>
2015-08-31 13:16:01 -0400

    Update actorgrab.py

--- actorgrab.py
+++ actorgrab.py
@@ -10,17 +10,15 @@ for actors in actorlist:
 	actors.replace(" ", "%20")
 	geturl = ("http://www.myapifilms.com/imdb?name=%s&format=JSON&filmography=1&limit=1&lang=en-us&exactFilter=0&bornDied=0&starSign=0&uniqueName=0&actorActress=0&actorTrivia=0&actorPhotos=N&actorVideos=N&salary=0&spouses=0&tradeMark=0&personalQuotes=0") % actors
 	response = requests.get(geturl)
-		#,
-	     #                    auth=('user', 'password'))
 	data = response.json()
-
-	#pprint(data)
-
-	#print (data[0]['filmographies'][0]['filmography'][1]['IMDBId'])
-
-
 
 	for filmog in data[0]['filmographies'][0]['filmography']:
 		print filmog['IMDBId']
-		addmovie = requests.get('http://localhost:5050/api/<api key>/movie.add/?identifier=' + filmog['IMDBId'])
-		print addmovie
+		if 'remarks' not in filmog:
+			addmovie = requests.get('http://localhost:5050/api/<api key/movie.add/?identifier=' + filmog['IMDBId'])
+			print addmovie
+		else:
+			 if ( '(TV Movie)' in filmog['remarks'] or '(TV Mini-Series)' in filmog['remarks'] ):
+			 	addmovie = requests.get('http://localhost:5050/api/<api key'/movie.add/?identifier=' + filmog['IMDBId'])
+			 	print addmovie
+			
