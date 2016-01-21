42297b0aa7cf893d38f17d579574b79fb6341384
mgaulton <mgaulton@users.noreply.github.com>
2016-01-21 13:38:44 -0500

    changes for v2 of the myapifilms api
    -token now required, get your own
    -returned json has changed

--- actorgrab.py
+++ actorgrab.py
@@ -7,18 +7,13 @@ actorlist = ['alicia witt','nathan fillion','richard dean anderson']
 
 
 for actors in actorlist:
-	actors.replace(" ", "%20")
-	geturl = ("http://www.myapifilms.com/imdb?name=%s&format=JSON&filmography=1&limit=1&lang=en-us&exactFilter=0&bornDied=0&starSign=0&uniqueName=0&actorActress=0&actorTrivia=0&actorPhotos=N&actorVideos=N&salary=0&spouses=0&tradeMark=0&personalQuotes=0") % actors
+	actors.replace(" ", "+")
+	geturl = ("http://www.myapifilms.com/imdb/idIMDB?name=%s&token=<API TOKEN>&format=json&language=en-us&filmography=1&exactFilter=0&limit=1&bornDied=0&starSign=0&uniqueName=0&actorActress=0&actorTrivia=0&actorPhotos=0&actorVideos=0&salary=0&spouses=0&tradeMark=0&personalQuotes=0&starMeter=0") % actors
 	response = requests.get(geturl)
-	data = response.json()
-
-	for filmog in data[0]['filmographies'][0]['filmography']:
-		print filmog['IMDBId']
-		if 'remarks' not in filmog:
-			addmovie = requests.get('http://localhost:5050/api/<api key/movie.add/?identifier=' + filmog['IMDBId'])
-			print addmovie
-		else:
-			 if ( '(TV Movie)' in filmog['remarks'] or '(TV Mini-Series)' in filmog['remarks'] ):
-			 	addmovie = requests.get('http://localhost:5050/api/<api key'/movie.add/?identifier=' + filmog['IMDBId'])
-			 	print addmovie
-			
+	data2 = response.json()
+	for filmoger in data2['data']['names'][0]['filmographies']:
+		print filmoger['section']
+		if 'Act' in filmoger['section']:
+			for filmog in filmoger['filmography']:
+				addmovie = requests.get('http://localhost:5050/api/<CP API>/movie.add/?identifier=' + filmog['imdbid'])
+				print  '\t',filmog['title'],'\t\t\t',filmog['remarks'],'\t', addmovie
